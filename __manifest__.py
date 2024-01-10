49d686d3e53bd5208dbad6df5a0533fa6bce434e
Sojib Mondol <mdsojibmandol@gmail.com>
2024-01-10 22:52:30 +0600

    estate menu chreated

--- __manifest__.py
+++ __manifest__.py
@@ -14,8 +14,8 @@ Make the lock date irreversible:
     """,
     'depends': [],
     'data':[
-        'security/ir.model.access.csv',
-        
+        # 'views/estate_property_views.xml',
+        'views/estate_menus.xml',
     ],
     'demo': [],
     'installable': True,
