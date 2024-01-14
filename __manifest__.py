a03c60fad87bbc78a9c3135b6bcefe56d9187f6b
Md. Mohiuddin <mohiuddin.ice.ru@gmail.com>
2024-01-14 10:42:03 +0600

    updated manfest

--- __manifest__.py
+++ __manifest__.py
@@ -1,16 +1,18 @@
 {
-    "name": "estate",
-    "version": "1.0",
+    "name": "Estate Property",
+    "version": "17.0.0.1",
     "category": "Productivity",
     "sequence": 2,
+    "author": "Sojib Mondol from Metamorphosis Limited",
     "description": """
         Estate Property Model
     """,
     "depends": [],
     "data": [
+        'views/estate_menus.xml',
         "security/ir.model.access.csv",
         'views/estate_property_views.xml',
-        'views/estate_menus.xml',
+        'views/estate_property_type_views.xml',
     ],
     "demo": [],
     "installable": True,
