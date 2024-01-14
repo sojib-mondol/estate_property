a9f112563712b45561accc4498073aac820136e9
Md. Mohiuddin <mohiuddin.ice.ru@gmail.com>
2024-01-14 10:42:51 +0600

    estate and models updated

--- models/estate_property.py
+++ models/estate_property.py
@@ -33,6 +33,13 @@ class EstateProperty(models.Model):
         readonly=True,
     )
 
+    best_price = fields.Integer("Best Price", compute="_compute_best_price")
+    property_type_id = fields.Many2one("estate.property.type", string="Property Type")
+    buyer_id = fields.Many2one("res.users", string="Buyer", default= lambda self: self.env.uid)
+    salesperson_id = fields.Many2one("res.partner", string="Sales person")
+
+    @api.depends("best_price")
+
     @api.depends("living_area", "garden_area")
     def _compute_total_area(self):
         for property_record in self:
@@ -62,10 +69,10 @@ class EstateProperty(models.Model):
     )
 
 
-def action_cancel_property(self):
-    if self.state == "sold":
-        raise exceptions.UserError("You cannot cancel a property that has been sold.")
-    self.state = "canceled"
+# def action_cancel_property(self):
+#     if self.state == "sold":
+#         raise exceptions.UserError("You cannot cancel a property that has been sold.")
+#     self.state = "canceled"
 
 
 def action_sell_property(self):
